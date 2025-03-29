5809ca72e22358f3a206970e92c45bcfbaf20241
Youssef-BS <youssefbensaid827@gmail.com>
2025-03-29 08:22:07 +0100

    complete service component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import "./portfolio.css"
+import { link } from 'motion/react-client'
+
+
+const items = [
+  { id: 1, img :"/super-market.png" , title: "Full Stack e-commerce application , online Marketplace for Buying Items .", description: "An e-commerce marketplace allowing sellers to list their products and users to make purchases through a secure and user-friendly platform. The site includes a product management system, shopping cart, online payment, and order tracking features." , link : "/" },
+  { id: 2, img :"/e-commerce-old-new.png" ,title: "Online Marketplace for Buying & Selling New & Used Items", description: "Developed a modern e-commerce platform where users can buy and sell new or second-hand clothes, electronics, accessories, and more" , link : "/" },
+  { id: 3, img :"/visual-audio.png" , title: "E-Commerce Website for Buying Audio & Visual Products", description: "Developed a full-featured e-commerce platform for purchasing audio and visual products, offering a seamless shopping experience" , link : "/" },
+  { id: 4, img :"/transform.png", title: "Home Moving & Item Exchange Platform", description: "Developed a web platform that allows users to exchange items, move house, and manage relocations efficiently." , link : "/" },
+]
+
+
+const ListItems = ({ items }) => {
+  return (
+    <div className='pItem'>
+      <div className="pImg">
+        <img src={items.img} alt="" />
+      </div>
+      <div className="pText">
+        <h1>{items.title}</h1>
+        <p>{items.description}</p>
+        <a href={items.link}>
+          <button className='pBtn'>View Project</button>
+        </a>
+      </div>
+    </div>
+  )
+}
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <div className='portfolio'>
+      <div className="pList">
+        {items.map((item) => (
+          <ListItems key={item.id} items={item} />
+        ))}
+      </div>
+    </div>
   )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,4 +1,4 @@
-import React , {useRef} from 'react'
+import React , {useRef , useState} from 'react'
 import "./service.css"
 import Counter from './Counter';
 import { motion, useInView } from 'motion/react';
@@ -59,6 +59,7 @@ const services = [
 
 const Services = () => {
 
+  const [option , setOption] = useState("web");
   const ref = useRef();
   const isInView = useInView(ref , {margin : "-200px"});
 
@@ -79,6 +80,7 @@ const Services = () => {
           {services.map((service) => (
               <motion.div className="service" key={service.id}
               variants={listVariants}
+              onClick={() => setOption(service.title.split(' ')[0].toLowerCase())}
               >
                 <div className="serviceIcon">
                   <img src={service.img} alt={service.title} />
@@ -96,10 +98,44 @@ const Services = () => {
         </div>
       </div>
       <div className='sSection right'>
-         <Technical icons='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwIy-mzDNwEgiWKpwsy_8CK9KSr6GEnCcpgQ&s' title='Next' />
-         <Technical icons='https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png' title='React' />
-         <Technical icons='https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png' title='Angular' />
-      </div>
+  {option === "web" ? 
+    <motion.div 
+      className="web"
+      variants={listVariants}
+      initial="initial"
+      animate={isInView ? "animate" : "initial"}
+    >
+      <Technical icons='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwIy-mzDNwEgiWKpwsy_8CK9KSr6GEnCcpgQ&s' title='Next' />
+      <Technical icons='https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png' title='React' />
+      <Technical icons='https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png' title='Angular' />
+      <Technical icons='https://www.manektech.com/storage/developer/1646733543.webp' title='Express' />
+      <Technical icons='https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Spring_Boot.svg/1200px-Spring_Boot.svg.png' title='SpringBoot' />
+    </motion.div> 
+  : option === "mobile" ? 
+    <motion.div 
+      className="mobile"
+      variants={listVariants}
+      initial="initial"
+      animate={isInView ? "animate" : "initial"}
+    >
+      <Technical icons='https://w7.pngwing.com/pngs/537/866/png-transparent-flutter-hd-logo.png' title='Flutter' />
+      <Technical icons='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png' title='ReactNative' />
+      <Technical icons='https://www.manektech.com/storage/developer/1646733543.webp' title='Express' />
+      <Technical icons='https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Spring_Boot.svg/1200px-Spring_Boot.svg.png' title='SpringBoot' />
+    </motion.div> 
+  : 
+    <motion.div 
+      className="desktop"
+      variants={listVariants}
+      initial="initial"
+      animate={isInView ? "animate" : "initial"}
+    >
+      <Technical icons='https://www.qftest.com/fileadmin/Webdata/logos-icons/JavaFX.png' title='JavaFx' />
+      <Technical icons='https://upload.wikimedia.org/wikipedia/fr/thumb/f/fe/SceneBuilderLogo.png/640px-SceneBuilderLogo.png' title='SceneBuilder' />
+    </motion.div>
+  }
+</div>
+
     </div>
   )
 }
